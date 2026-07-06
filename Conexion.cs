3909b32e7e281da0c24330e2dae02eb2517e769c
Yesenia Magdalena <yesenia21th55@gmail.com>
2026-07-05 19:56:07 -0600

    Configuración de la clase Conexion.cs con MySQL

---
+++ Conexion.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Punto
+{
+    internal class Conexion
+    {
+        private readonly string cadenaConexion = "Server=localhost;Database=PuntoDB;User ID=root;Password=;Port=3306;SslMode=none";
+
+        public MySqlConnection obtenerConexion()
+        {
+            MySqlConnection conectar = new MySqlConnection(cadenaConexion);
+            try
+            {
+                conectar.Open();
+                return conectar;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al conectar con la base de datos MSQL" + ex.Message, "Error de conexión", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+    }
+}
